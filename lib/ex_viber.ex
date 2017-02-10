1fc4839f77709d63848ccecd3306e692ea8b577b
Ciarán Walsh <git@ciaranwal.sh>
2017-02-10 13:25:18 +0000

    Initial commit

---
+++ lib/ex_viber.ex
@@ -0,0 +1,18 @@
+defmodule ExViber do
+  @moduledoc """
+  Documentation for ExViber.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ExViber.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/ex_viber_test.exs
@@ -0,0 +1,8 @@
+defmodule ExViberTest do
+  use ExUnit.Case
+  doctest ExViber
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
